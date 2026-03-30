c9706560e546b3f96acc6d9985749a4a854598bb
Akaash <mkaksh0@gmail.com>
2026-03-30 17:02:24 +0530

    commit

---
+++ src/MIDTERM_1_22MIC0165/main.java
@@ -0,0 +1,9 @@
+package MIDTERM_1_22MIC0165;
+
+public class main {
+public static void main(String[] args) {
+System.out.println("Student Name: Mandeep Singh");
+System.out.println("Register Number: 22MIC0165");
+System.out.println("Message: Jenkins Build Successful");
+}
+}
